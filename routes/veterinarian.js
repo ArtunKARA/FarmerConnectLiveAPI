c67a73131f8e7a4c94858a0f070215822dab4dbe
ArtunKARA <artunankara@gmail.com>
2024-05-10 15:32:24 +0300

    veterian

--- routes/veterinarian.js
+++ routes/veterinarian.js
@@ -2,24 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  veterinarianRequestsSituation,
-  getVeterinarianRequestsStatus,
   getFarmerVeterinarianData,
+  getAllVeterinarianRequests,
   getVeterinarianVeterinarianData,
   getVeterinarianDataDetails,
   setFarmerVeterinarianRequest,
   setVeterinarianRequestAproved,
   setVeterinarianRequestDiagnosis,
+  setVeterinarianRequestCanceled,
 } = require("../controllers/veterinarian-controller.js");
 
-router.get("/situation", veterinarianRequestsSituation);
-router.get("/status", getVeterinarianRequestsStatus);
-router.get("/farmer/:mail", getFarmerVeterinarianData);
-router.get("/veterinarian/:mail", getVeterinarianVeterinarianData);
-router.get("/details", getVeterinarianDataDetails);
-router.post("/farmerRequest", setFarmerVeterinarianRequest);
-router.post("/veterinarianRequestAproved", setVeterinarianRequestAproved);
-router.post("/veterinarianRequestDiagnosis", setVeterinarianRequestDiagnosis);
+
+router.get("/farmer/:mail", getFarmerVeterinarianData); // Çiftiçinin veriseine göre seriner istekleri getirir  (urlde mail bilgisi olmalı)
+router.get("/all", getAllVeterinarianRequests); // Tüm veteriner isteklerini getirir veteriner üstüne alımmamış olanlar
+router.get("/veterinarian/:mail", getVeterinarianVeterinarianData); // Veterinerin veriseine göre seriner istekleri getirir  (urlde mail bilgisi olmalı)
+router.get("/details/:id", getVeterinarianDataDetails); // Veteriner isteğinin detaylarını getirir (urlde talep id bilgisi olmalı)
+router.post("/farmerRequest", setFarmerVeterinarianRequest); // Çiftçinin veteriner isteği oluşturmasını sağlar (bodyde mail ve status bilgisi olmalı)
+router.post("/veterinarianRequestAproved", setVeterinarianRequestAproved); // Veteriner isteğini onaylar (bodyde talep id bilgisi ve onaycı mail bilgisi olmalı)
+router.post("/veterinarianRequestDiagnosis", setVeterinarianRequestDiagnosis); // Veteriner isteğine tanı koyar (bodyde talep id ve tanı bilgisi olmalı)
+router.get("/veterinarianRequestCanceled/:id", setVeterinarianRequestCanceled); // Veteriner isteğini iptal eder (urlde talep id bilgisi olmalı)
 
 
 module.exports = router;
